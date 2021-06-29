b09a76285e3ccf630d1e06772a7ca7dc129e513f
wotsushi <wotsushi@gmail.com>
2021-06-29 21:42:28 +0900

    ナビゲーションバーを追加

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,34 @@
 import './App.css';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import { Navbar, Nav } from 'react-bootstrap';
 import MonsterTable from './Monster';
 import SpellTable from './Spell';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return (
-    <div className="App">
-      <MonsterTable />
-      <SpellTable />
-    </div>
+    <Router>
+      <div className="App">
+        <Navbar bg="light" expand="lg">
+          <Navbar.Brand href="/">04環境</Navbar.Brand>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="mr-auto">
+              <Nav.Link href="/monster">モンスター</Nav.Link>
+              <Nav.Link href="/spell">魔法</Nav.Link>
+            </Nav>
+          </Navbar.Collapse>
+        </Navbar>
+        <Switch>
+          <Route path="/monster">
+            <MonsterTable />
+          </Route>
+          <Route path="/spell">
+            <SpellTable />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
