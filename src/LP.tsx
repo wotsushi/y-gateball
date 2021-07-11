f6492ec3dd60bec108cb3bd162cdddab333611a8
wotsushi <wotsushi@gmail.com>
2021-07-11 21:12:04 +0900

    ライフポイントのデザイン修正
    ISSUE: #27

--- src/LP.tsx
+++ src/LP.tsx
@@ -1,4 +1,4 @@
-import { Button, Container, Col, Row } from "react-bootstrap";
+import { Button, Container, Col, Row, ProgressBar } from "react-bootstrap";
 
 const toStringWithSign = (x: number) => {
   if (x > 0) {
@@ -13,6 +13,27 @@ const lifeValues = [
   [-600, -800, -1000],
   [-2000, -3000, 1000],
 ];
+
+const LifePoint = (props: { lp: number }) => {
+  const { lp } = props;
+  const now = Math.floor(lp / 80);
+  const variant = (() => {
+    if (lp > 4000) {
+      return "success";
+    } else if (lp > 2000) {
+      return "warning";
+    } else {
+      return "danger";
+    }
+  })();
+  return (
+    <div className="lp-parent bg-light text-black">
+      <div className="lp-header">LP</div>
+      <ProgressBar variant={variant} now={now}></ProgressBar>
+      <div className="lp">{props.lp}</div>
+    </div>
+  );
+};
 
 const ControlPanel = () => {
   return (
@@ -43,9 +64,19 @@ const LP = () => {
   return (
     <Container>
       <Row>
-        <Col>LP: 8000 先攻</Col>
+        <Col>
+          <div className="player">
+            <LifePoint lp={8000}></LifePoint>
+            <div>先攻</div>
+          </div>
+        </Col>
         <Col>[New] [←] [→] [履歴]</Col>
-        <Col>LP: 4700 後攻</Col>
+        <Col>
+          <div className="player">
+            <div>後攻</div>
+            <LifePoint lp={3000}></LifePoint>
+          </div>
+        </Col>
       </Row>
       <Row>
         <Col>
